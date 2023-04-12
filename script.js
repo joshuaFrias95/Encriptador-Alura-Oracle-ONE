5accc819e9b648a5320da5106b4d5f925a6465fe
joshuaFrias95 <metatron1495@gmail.com>
2023-04-11 20:11:33 -0600

    Se agrega fichero script.js con la funcionalidad de typescript en el título

---
+++ script.js
@@ -0,0 +1,18 @@
+//Typed.js
+
+const typed = new Typed('.typed', {
+  strings: ['<i class="crypted">Encripta</i>',
+            '<i class="crypted">Desencripta</i>',
+            '<i class="crypted">Repite</i>'
+  ],
+  stringElement: '#cadenas-texto',
+  typeSpeed: 75,
+  startDelay: 300,
+  backSpeed: 75,
+  shuffle: false,
+  backDelay: 1000,
+  loop: true,
+  loopCount: false,
+  showCursor: true,
+  cursorChar: '_'
+});
