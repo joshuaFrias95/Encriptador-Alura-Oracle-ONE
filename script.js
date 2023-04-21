b0f036ed526f285a26be0c14ddfabff903241ae6
joshuaFrias95 <metatron1495@gmail.com>
2023-04-20 22:25:55 -0600

    Actualización y revisión de lógica

--- script.js
+++ script.js
@@ -1,3 +1,84 @@
+// Lógica de encriptación y desencriptación
+const textArea = document.querySelector('textarea');
+const btnC = document.getElementById('btn-c'); 
+const btnE = document.getElementById('btn-e');
+const btnD = document.getElementById('btn-d');
+const result = document.getElementById('showResult');
+
+let encripta = {
+  'a': 'ai',
+  'e': 'enter',
+  'i': 'imes',
+  'o': 'ober',
+  'u': 'ufai'
+}
+
+let codigos= {
+  'ai': 'a',
+  'enter': 'e',
+  'imes': 'i',
+  'ober': 'o',
+  'ufai': 'u'
+}; 
+
+//Reiniciamos valores del área de texto 
+textArea.value= "";
+textArea.focus();
+
+function cambiarClase() {
+  //oculta 
+  document.getElementById('doll').classList.add('oculto');
+  document.getElementById('titulo').classList.add('oculto');
+  //Muestra
+  document.getElementById('corazon').classList.remove('oculto');
+  document.getElementById('titulo2').classList.remove('oculto');
+  result.classList.remove('oculto');
+  result.classList.add('ajuste');
+  btnC.classList.remove('oculto');
+  btnC.classList.add('show');
+  
+}
+
+function mostrar(texto) {
+  result.innerHTML = texto;
+  
+}
+
+function encriptar() {
+  let texto = textArea.value.toLocaleLowerCase();
+  textArea.value = "";
+  
+  texto = texto.replace(/a|e|i|o|u/g, function(matched) {
+    return encripta[matched]
+  });
+  
+  cambiarClase();
+  mostrar(texto);
+}
+
+function desencriptar() {
+  let texto = textArea.value.toLocaleLowerCase();
+  textArea.value = "";
+  
+  texto = texto.replace(/ai|enter|imes|ober|ufai/g, function(matched) {
+    return codigos[matched]
+  });
+  
+  cambiarClase();
+  mostrar(texto);
+}
+
+function copiar() {
+  navigator.clipboard.writeText(result.innerHTML);
+}
+
+
+btnE.onclick = encriptar;
+btnD.onclick = desencriptar;
+btnC.onclick = copiar;
+
+
+
 //Typed.js
 
 const typed = new Typed('.typed', {
@@ -15,96 +96,4 @@ const typed = new Typed('.typed', {
   showCursor: true,
   cursorChar: '_'
 });
-
-
-
-// Lógica de encriptación y desencriptación
-
-const textArea = document.querySelector('textarea');
-const btnC = document.getElementById('btn-c');
-const btnE = document.getElementById('btn-e');
-const btnD = document.getElementById('btn-d');
-const result = document.getElementById('showResult');
- let encripta= {
- 'a': 'ai',
- 'e': 'enter',
- 'i': 'imes',
- 'o': 'ober',
- 'u': 'ufai'
- };
- 
- let codigos= {
- 'ai': 'a',
- 'enter': 'e',
- 'imes': 'i',
- 'ober': 'o',
- 'ufai': 'u'
- }; 
- 
-
-textArea.value = "";
-textArea.focus();
-
-
-
-function copiar() {
-  navigator.clipboard.writeText(result.innerHTML);
-}
-
-
-
-function cambiarClase() {
-
-  //Oculta
-  document.getElementById('doll').classList.add('oculto');
-  document.getElementById('subtitulo').classList.add('oculto');
-  document.getElementById('texto').classList.add('oculto');
-  //Muestra
-  document.getElementById('subtitulo2').classList.remove('oculto');
-  document.getElementById('copyarea').classList.remove('oculto');
-  result.classList.remove('oculto');
-  result.classList.add('ajuste');
-
-}
-
-
-
-function mostrar(texto) {
-  
-  document.getElementById('showResult').innerHTML = texto;
-}
-
-
-
-function encriptar() {
-  let texto = textArea.value.toLowerCase();
-  textArea.value = "";
-  
-  texto = texto.replace(/a|e|i|o|u/g, function(matched){
-    return encripta[matched]
-  });
-  
-  cambiarClase();
-  mostrar(texto);
-}
-
-
-function desencriptar() {
-  let texto = textArea.value.toLowerCase(); 
-  textArea.value = "";
-  
- texto = texto.replace(/ai|enter|imes|ober|ufai/g, function(matched){
-  return codigos[matched]
- });
- 
- cambiarClase();
- mostrar(texto);
-}
-
-
-
-btnC.onclick = copiar;
-btnE.onclick = encriptar;
-btnD.onclick = desencriptar;
-
 
